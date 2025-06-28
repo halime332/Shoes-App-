4da40eda1b7bbb47935a1dd6627d77d297601aa0
halime332 <halimeyavuz1001@gmail.com>
2025-06-28 21:23:49 +0300

    Soe App

--- client/src/components/detail-page/color.tsx
+++ client/src/components/detail-page/color.tsx
@@ -11,19 +11,23 @@ const Color:FC<Props> = ({item}) => {
   const [selected,setSelected] =useState<string>("");
 
   const toogle =(id:string)=>{
-    setSelected(selected=== id ? "":id);
-  }
+    setSelected(selected=== id ? "": id);
+  };
+
   return (
     <div>
       <h2 className="font-semibold mb-3">Renk Seçiniz</h2>
 
       <div className="flex gap-5">
          {item.color.split(",").map((id)=>{
+
+            //ekrana basılan rengin kodunu bul
              const color=colors.find((i)=>i.id ===id);
 
+              //ekrana basıla rengin kodunu bul
+             const isSelected =selected===id;
              return (
-              
-              <div>
+              <div className={isSelected ? "ring-3 ring-my-blue rounded-full":""}>
                 <div onClick={()=>toogle(id)} className="mt-1 size-9 rounded-full 
                 cursor-pointer" style={{background:color?.code}}/>
               </div>
